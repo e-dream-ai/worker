ed75d16347b929bfb406b718c97225bcc799e471
Max Carlson <>
2025-01-14 11:45:54 +0800

    Refactor to make a generic createWorker factory that logs all worker events, log progress and return values in prompt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Job, Worker } from 'bullmq';
 import 'dotenv/config';
 import { Redis } from 'ioredis';
-import runpodSdk, { EndpointOutput } from "runpod-sdk";
+import runpodSdk from 'runpod-sdk';
+
+const DEBUG = false;
 
 const { RUNPOD_API_KEY, ENDPOINT_ID } = process.env;
 const runpod = runpodSdk(RUNPOD_API_KEY || '');
@@ -11,137 +13,114 @@ const redisClient = new Redis({
   maxRetriesPerRequest: null,
 });
 
-// handle queued messages
-new Worker(
-  'runpod',
-  async (job: Job) => {
-    if (endpoint) {
-      console.log(`Starting job: ${JSON.stringify(job.data)}`);
-      const {id} = await endpoint.run({
-          "input":
-          {
-              "workflow":
-              {
-                  "3":
-                  {
-                      "inputs":
-                      {
-                          "seed": 1337,
-                          "steps": 20,
-                          "cfg": 8,
-                          "sampler_name": "euler",
-                          "scheduler": "normal",
-                          "denoise": 1,
-                          "model":
-                          [
-                              "4",
-                              0
-                          ],
-                          "positive":
-                          [
-                              "6",
-                              0
-                          ],
-                          "negative":
-                          [
-                              "7",
-                              0
-                          ],
-                          "latent_image":
-                          [
-                              "5",
-                              0
-                          ]
-                      },
-                      "class_type": "KSampler"
-                  },
-                  "4":
-                  {
-                      "inputs":
-                      {
-                          "ckpt_name": "sd_xl_base_1.0.safetensors"
-                      },
-                      "class_type": "CheckpointLoaderSimple"
-                  },
-                  "5":
-                  {
-                      "inputs":
-                      {
-                          "width": 512,
-                          "height": 512,
-                          "batch_size": 1
-                      },
-                      "class_type": "EmptyLatentImage"
-                  },
-                  "6":
-                  {
-                      "inputs":
-                      {
-                          "text": job.data.prompt,
-                          "clip":
-                          [
-                              "4",
-                              1
-                          ]
-                      },
-                      "class_type": "CLIPTextEncode"
-                  },
-                  "7":
-                  {
-                      "inputs":
-                      {
-                          "text": "text, watermark",
-                          "clip":
-                          [
-                              "4",
-                              1
-                          ]
-                      },
-                      "class_type": "CLIPTextEncode"
-                  },
-                  "8":
-                  {
-                      "inputs":
-                      {
-                          "samples":
-                          [
-                              "3",
-                              0
-                          ],
-                          "vae":
-                          [
-                              "4",
-                              2
-                          ]
-                      },
-                      "class_type": "VAEDecode"
-                  },
-                  "9":
-                  {
-                      "inputs":
-                      {
-                          "filename_prefix": "ComfyUI",
-                          "images":
-                          [
-                              "8",
-                              0
-                          ]
-                      },
-                      "class_type": "SaveImage"
-                  }
-              }
-          }
-      });
+const serializeError = (error: Error) => {
+  return JSON.stringify(error, Object.getOwnPropertyNames(error));
+};
 
-
-      let status;
-      do {
-        status = await endpoint.status(id);
-        console.log(`Got status: ${JSON.stringify(status)}`);
-      } while(status.status !== 'COMPLETED')
-
-      return status;
+function createWorker(name: string, handler) {
+  const worker = new Worker(
+    name,
+    async (job) => {
+      return await handler(job);
+    },
+    {
+      connection: redisClient,
     }
-  },
-  { connection: redisClient }
-);
+  );
+
+  worker.on('failed', (job, error: Error) => {
+    console.error(`Worker failed: ${name} error: ${serializeError(error)}, job data: ${JSON.stringify(job?.toJSON())}`);
+  });
+
+  worker.on('completed', (job, returnvalue) => {
+    if (DEBUG)
+      console.debug(
+        `Job completed: {name}, returning: ${JSON.stringify(returnvalue)}, job data: ${JSON.stringify(job.toJSON())}`
+      );
+  });
+
+  worker.on('error', (error: Error) => {
+    console.error(`Job error:     ${name} error:${serializeError(error)}`);
+  });
+}
+
+async function runpodJob(job: Job) {
+  if (endpoint) {
+    if (DEBUG) console.log(`Starting runpod worker: ${JSON.stringify(job.data)}`);
+    const { id } = await endpoint.run({
+      input: {
+        workflow: {
+          '3': {
+            inputs: {
+              seed: 1337,
+              steps: 20,
+              cfg: 8,
+              sampler_name: 'euler',
+              scheduler: 'normal',
+              denoise: 1,
+              model: ['4', 0],
+              positive: ['6', 0],
+              negative: ['7', 0],
+              latent_image: ['5', 0],
+            },
+            class_type: 'KSampler',
+          },
+          '4': {
+            inputs: {
+              ckpt_name: 'sd_xl_base_1.0.safetensors',
+            },
+            class_type: 'CheckpointLoaderSimple',
+          },
+          '5': {
+            inputs: {
+              width: 512,
+              height: 512,
+              batch_size: 1,
+            },
+            class_type: 'EmptyLatentImage',
+          },
+          '6': {
+            inputs: {
+              text: job.data.prompt,
+              clip: ['4', 1],
+            },
+            class_type: 'CLIPTextEncode',
+          },
+          '7': {
+            inputs: {
+              text: 'text, watermark',
+              clip: ['4', 1],
+            },
+            class_type: 'CLIPTextEncode',
+          },
+          '8': {
+            inputs: {
+              samples: ['3', 0],
+              vae: ['4', 2],
+            },
+            class_type: 'VAEDecode',
+          },
+          '9': {
+            inputs: {
+              filename_prefix: 'ComfyUI',
+              images: ['8', 0],
+            },
+            class_type: 'SaveImage',
+          },
+        },
+      },
+    });
+
+    let status;
+    do {
+      status = await endpoint.status(id);
+      if (DEBUG) console.log(`Got status: ${JSON.stringify(status)}`);
+      await job.updateProgress(status);
+    } while (status.status !== 'COMPLETED');
+
+    // return S3 url to result
+    return JSON.parse(JSON.stringify(status)).output.message;
+  }
+}
+
+createWorker('image', runpodJob);
--- src/prompt.ts
+++ src/prompt.ts
@@ -1,5 +1,32 @@
-import { Job, Queue } from 'bullmq';
+import { Job, Queue, QueueEvents } from 'bullmq';
 import { Redis } from 'ioredis';
+
+const redisClient = new Redis({
+  maxRetriesPerRequest: null,
+});
+
+// Queue to add jobs
+const runpodQueue = new Queue('image', { connection: redisClient });
+async function run(prompt: string) {
+  return await runpodQueue.addBulk([
+    {
+      name: 'message',
+      data: {
+        prompt,
+      },
+    },
+  ]);
+}
+
+// listen for job events and results. Note that it's best to deal with results directly in the worker
+const queueEvents = new QueueEvents('image');
+queueEvents.on('completed', async (data) => {
+  const job = await Job.fromId(runpodQueue, data.jobId);
+  console.log(`Job finished: ${JSON.stringify(job?.returnvalue)}`);
+});
+queueEvents.on('progress', (data) => {
+  console.log(`Job progress: ${JSON.stringify(data)}`);
+});
 
 if (process.argv.length === 2) {
   console.error('Expected at least one argument!');
@@ -7,27 +34,4 @@ if (process.argv.length === 2) {
 }
 
 const prompt = process.argv.slice(2).join(' ');
-
-const redisClient = new Redis({
-  maxRetriesPerRequest: null,
-});
-
-// handle queued messages
-var runpodQueue = new Queue(
-  'runpod',
-  { connection: redisClient }
-);
-
-
-async function run(prompt:string) {
-  await runpodQueue.addBulk([
-    {
-      name: 'runpod',
-      data: {
-        prompt
-      },
-    }
-  ])
-}
-
-run(prompt).then(() => process.exit())
+await run(prompt);
