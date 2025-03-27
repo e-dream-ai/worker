f21a9ab111089cbb40103c677079a3641abc606a
Max Carlson <>
2025-03-27 21:36:23 +0800

    read comma-separated values for keyframes if provided, improved console output

--- src/prompt.ts
+++ src/prompt.ts
@@ -5,7 +5,7 @@ const redisClient = new Redis({
   maxRetriesPerRequest: null,
 });
 
-const runpodQueue = new Queue('video', {
+const videoQueue = new Queue('video', {
   connection: redisClient,
   defaultJobOptions: {
     attempts: 1,
@@ -16,42 +16,50 @@ const runpodQueue = new Queue('video', {
   },
 });
 
-async function run(firstFrame) {
-  return runpodQueue.addBulk([
-    {
-      name: 'message',
-      data: {
-        prompt: {
-          '0': firstFrame || 'cubist painting of the ayahuasca experience',
-          '50': 'layered pointillist mitochondria from dreamtime',
-          '100': 'rave detailed Abstract  spiritual  Paintings',
-          '150': 'abstract art based on Kabbalah astrological chart',
-          '200': 'intricate futuristic iridescent multicolored japanese radiolaria',
-          '250': 'DMT painting android bio nano techno',
-          '300': firstFrame || 'cubist painting of the ayahuasca experience',
-        },
-        pre_text: 'highly detailed, 4k, masterpiece',
-        print_output: '(Masterpiece, best quality:1.2)  walking towards camera, full body closeup shot',
-        frame_count: 24,
-        frame_rate: 8,
+async function runVideo(frames: string[]) {
+  // console.log(`frames: ${JSON.stringify(frames)}`)
+  const job = {
+    name: 'message',
+    data: {
+      prompt: {
+        '0': frames[0] || 'cubist painting of the ayahuasca experience',
+        '50': frames[1] || 'layered pointillist mitochondria from dreamtime',
+        '100': frames[2] || 'rave detailed Abstract  spiritual  Paintings',
+        '150': frames[3] || 'abstract art based on Kabbalah astrological chart',
+        '200': frames[4] || 'intricate futuristic iridescent multicolored japanese radiolaria',
+        '250': frames[5] || 'DMT painting android bio nano techno',
+        '304': frames[0] || 'cubist painting of the ayahuasca experience',
       },
+      pre_text: 'highly detailed, 4k, masterpiece',
+      print_output: '(Masterpiece, best quality:1.2)  walking towards camera, full body closeup shot',
+      frame_count: 304, // should be a multiple of the context window of 16
+      frame_rate: 8,
     },
-  ]);
+  };
+
+  console.log(`running: ${JSON.stringify(job)}`);
+  return videoQueue.addBulk([job]);
 }
 
 // listen for job events and results. Note that it's best to deal with results directly in the worker
 const queueEvents = new QueueEvents('video');
 queueEvents.on('completed', async (data) => {
-  const job = await Job.fromId(runpodQueue, data.jobId);
-  console.log(`Job finished: ${JSON.stringify(job?.returnvalue)}`);
+  const job = await Job.fromId(videoQueue, data.jobId);
+  console.log(`Job finished: ${JSON.stringify(job?.returnvalue)} for job ${JSON.stringify(job)}`);
   process.exit();
 });
-queueEvents.on('progress', async (data) => {
-  const job = await Job.fromId(runpodQueue, data.jobId);
-  console.log(`Job progress: ${JSON.stringify(data)} for job ${JSON.stringify(job)}`);
+let lastprogress = '';
+queueEvents.on('progress', (data) => {
+  const progress = JSON.stringify(data);
+  if (lastprogress != progress) {
+    console.log(`Job progress: ${progress}`);
+    lastprogress = progress;
+  } else {
+    process.stdout.write('.');
+  }
 });
 queueEvents.on('failed', async (data) => {
-  const job = await Job.fromId(runpodQueue, data.jobId);
+  const job = await Job.fromId(videoQueue, data.jobId);
   console.log(`Job failed:   ${job.failedReason} for job ${JSON.stringify(job)}`);
 });
 
@@ -61,4 +69,4 @@ if (process.argv.length === 2) {
 }
 
 const prompt = process.argv.slice(2).join(' ');
-await run(prompt);
+await runVideo(prompt.split(','));
