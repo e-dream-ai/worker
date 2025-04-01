996cb6d174e591d06a4c3fff56c5b10c4a64bf50
Max Carlson <>
2025-04-02 00:51:54 +0800

    Add redisClient

---
+++ src/shared/redis.ts
@@ -0,0 +1,15 @@
+import Redis from 'ioredis';
+import env from './env.js';
+
+const redisClient = env.REDISCLOUD_URL
+  ? new Redis(env.REDISCLOUD_URL, {
+      maxRetriesPerRequest: null,
+    })
+  : new Redis({
+      host: env.REDIS_HOST,
+      port: env.REDIS_PORT,
+      password: env.REDIS_PASSWORD,
+      maxRetriesPerRequest: null,
+    });
+
+export default redisClient;
