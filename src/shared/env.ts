84d4846ee4f9043122dd7f5bc24852397e9beffd
Max Carlson <>
2025-04-02 00:51:29 +0800

    Add environment parsing

---
+++ src/shared/env.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import { bool, cleanEnv, port, str } from 'envalid';
+
+export const env = cleanEnv(process.env, {
+  NODE_ENV: str({ default: 'debug' }),
+  PORT: port({ default: 3000 }),
+  DEBUG: bool({ default: false }),
+
+  /**
+   * REDIS
+   */
+  // REDISCLOUD_URL is set by heroku automatically
+  REDISCLOUD_URL: str({ devDefault: '' }),
+  REDIS_HOST: str({ devDefault: 'localhost' }),
+  REDIS_PORT: port({ devDefault: 6379 }),
+  REDIS_PASSWORD: str({ devDefault: '' }),
+
+  RUNPOD_API_KEY: str(),
+  RUNPOD_ENDPOINT_ID: str(),
+});
+
+export default env;
