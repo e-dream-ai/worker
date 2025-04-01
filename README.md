734de69efeddfc11e7330716679c36c2ce572c3b
mcarlson <max@maxcarlson.com>
2025-04-01 22:25:54 +0800

    video works
    * Add BPS video generation workflow, run it by default
    
    * Use API format save from comfyui
    
    * Apparently the internal API changed?
    
    * Improve error handling
    
    * Move video prompts to prompt.js
    
    * Exit when the job finishes.
    
    * Fix start and end transitions.
    
    * Allow more time for stalled jobs
    
    * User job id for filename prefix, better typing, logging and error handling
    
    * Add explicit defaults for retries
    
    * Improve queue event logging
    
    * Throw errors when there are no results
    
    * read comma-separated values for keyframes if provided, improved console output
    
    * Don't fail when there's an exception calling runpod
    
    * Update README.md
    
    * Better error handling, list active jobs on startup
    
    * add package-lock.json
    
    * Standardize seed, steps and filename_prefix args
    
    * Add bull board
    
    * Update README.md
    
    * Add job logging, DRY up status checking
    
    * Improve prompting by allowing last keyframe to be overridden and trimming values, expose seed steps and motion_scale parameters, better logging
    
    * Don't set stalledInterval
    
    * Add runpod_id to the job data
    
    * test
    
    * Parameterize generated image size
    
    * Add runpod_id for image generation also
    
    ---------
    
    Co-authored-by: Max Carlson <>
    Co-authored-by: Scott Draves <spot@draves.org>

--- README.md
+++ README.md
@@ -7,9 +7,16 @@
 3. `npm install`
 4. `brew install redis`
 5. Grab the appropriate runpod .env from 1Password and save to `.env`
+6. `npm run build`
 
 ## Running
 
 1. `nvm use`
-2. `npm start` to start the worker or use `npm run dev` to recompile/restart automatically
-3. `node dist/prompt.js A Fish in a skyscraper` to execute a job with a prompt
+2. `npm start &` to start the worker and server
+3. visit http://localhost:3000/admin/queues to view job queues
+4. `node dist/prompt.js A fish on a bicycle` to execute a job with a prompt OR `node dist/prompt.js keyframe1, keyframe2, keyframe3` to replace multiple keyframes with comma-separated values, see
+
+## Ops
+
+1. Visit https://www.runpod.io/console/serverless/user/endpoint/89vs9h0qx0g966?tab=requests to see recent activity and cancel jobs you don't wish to keep running.
+2. https://apps.apple.com/us/app/red-2-ui-for-redis/id1491764008?mt=12 is helpful for seeing what's in Redis
--- src/prompt.ts
+++ src/prompt.ts
@@ -5,27 +5,68 @@ const redisClient = new Redis({
   maxRetriesPerRequest: null,
 });
 
-// Queue to add jobs
-const runpodQueue = new Queue('image', { connection: redisClient });
-async function run(prompt: string) {
-  return await runpodQueue.addBulk([
-    {
-      name: 'message',
-      data: {
-        prompt,
-      },
+const videoQueue = new Queue('video', {
+  connection: redisClient,
+  defaultJobOptions: {
+    attempts: 1,
+    backoff: {
+      type: 'exponential',
+      delay: 1000,
     },
-  ]);
+  },
+});
+
+async function runVideo(frames: string[]) {
+  // console.log(`frames: ${JSON.stringify(frames)}`)
+  const job = {
+    name: 'message',
+    data: {
+      prompt: {
+        '0': frames[0]?.trim() || 'cubist painting of the ayahuasca experience',
+        '100': frames[1]?.trim() || 'layered pointillist mitochondria from dreamtime',
+        '200': frames[2]?.trim() || 'rave detailed Abstract  spiritual  Paintings',
+        '300': frames[3]?.trim() || 'abstract art based on Kabbalah astrological chart',
+        '400': frames[4]?.trim() || 'intricate futuristic iridescent multicolored japanese radiolaria',
+        '500': frames[5]?.trim() || 'DMT painting android bio nano techno',
+        '608': frames[6]?.trim() || frames[0]?.trim() || 'cubist painting of the ayahuasca experience',
+      },
+      pre_text: 'highly detailed, 4k, masterpiece',
+      print_output: '(Masterpiece, best quality:1.2)  walking towards camera, full body closeup shot',
+      frame_count: 608, // should be a multiple of the context window of 16
+      frame_rate: 16,
+      seed: 832386334143550,
+      steps: 30,
+      motion_scale: 1,
+      width: 1024,
+      height: 768,
+    },
+  };
+
+  console.log(`running: ${JSON.stringify(job)}`);
+  return videoQueue.addBulk([job]);
 }
 
 // listen for job events and results. Note that it's best to deal with results directly in the worker
-const queueEvents = new QueueEvents('image');
+const queueEvents = new QueueEvents('video');
 queueEvents.on('completed', async (data) => {
-  const job = await Job.fromId(runpodQueue, data.jobId);
-  console.log(`Job finished: ${JSON.stringify(job?.returnvalue)}`);
+  const job = await Job.fromId(videoQueue, data.jobId);
+  console.log(
+    `\n${new Date().toISOString()}: Job finished: ${JSON.stringify(job?.returnvalue)} for job ${JSON.stringify(job)}`
+  );
 });
+let lastprogress = '';
 queueEvents.on('progress', (data) => {
-  console.log(`Job progress: ${JSON.stringify(data)}`);
+  const progress = JSON.stringify(data);
+  if (lastprogress != progress) {
+    console.log(`\n${new Date().toISOString()}: Job progress: ${progress}`);
+    lastprogress = progress;
+  } else {
+    process.stdout.write('.');
+  }
+});
+queueEvents.on('failed', async (data) => {
+  const job = await Job.fromId(videoQueue, data.jobId);
+  console.log(`\n${new Date().toISOString()}: Job failed:   ${job.failedReason} for job ${JSON.stringify(job)}`);
 });
 
 if (process.argv.length === 2) {
@@ -34,4 +75,4 @@ if (process.argv.length === 2) {
 }
 
 const prompt = process.argv.slice(2).join(' ');
-await run(prompt);
+await runVideo(prompt.split(','));
