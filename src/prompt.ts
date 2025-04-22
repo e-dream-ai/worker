b60c48fa10a469b21e29ac2e68fcd02d20e6f424
Max Carlson <>
2025-04-23 01:53:18 +0800

    Add command-line arg processing for both video types

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,7 +1,8 @@
 import { Job, Queue, QueueEvents } from 'bullmq';
 import redisClient from './shared/redis.js';
+import { InvalidArgumentError, program } from 'commander';
 
-const videoQueue = new Queue('hunyuanvideo', {
+const videoQueue = new Queue('video', {
   connection: redisClient,
   defaultJobOptions: {
     attempts: 1,
@@ -12,21 +13,29 @@ const videoQueue = new Queue('hunyuanvideo', {
   },
 });
 
-async function runVideo(frames: string[]) {
+async function runVideo(frames: string[], options) {
   // console.log(`frames: ${JSON.stringify(frames)}`)
   const job = {
     name: 'message',
     data: {
-      prompt: frames.join(),
-      pre_text: 'highly detailed, 4k, masterpiece',
-      print_output: '(Masterpiece, best quality:1.2)  walking towards camera, full body closeup shot',
-      frame_count: 608, // should be a multiple of the context window of 16
-      frame_rate: 16,
-      seed: 832386334143550,
-      steps: 30,
+      prompt: {
+        '0': frames[0]?.trim() || 'cubist painting of the ayahuasca experience',
+        '100': frames[1]?.trim() || 'layered pointillist mitochondria from dreamtime',
+        '200': frames[2]?.trim() || 'rave detailed Abstract  spiritual  Paintings',
+        '300': frames[3]?.trim() || 'abstract art based on Kabbalah astrological chart',
+        '400': frames[4]?.trim() || 'intricate futuristic iridescent multicolored japanese radiolaria',
+        '500': frames[5]?.trim() || 'DMT painting android bio nano techno',
+        '608': frames[6]?.trim() || frames[0]?.trim() || 'cubist painting of the ayahuasca experience',
+      },
+      pre_text: options.pre_text,
+      app_text: options.app_text,
+      frame_count: options.frame_count, // should be a multiple of the context window of 16
+      frame_rate: options.frame_rate,
+      seed: options.seed,
+      steps: options.steps,
+      width: options.width,
+      height: options.height,
       motion_scale: 1,
-      width: 1024,
-      height: 768,
     },
   };
 
@@ -34,10 +43,40 @@ async function runVideo(frames: string[]) {
   return videoQueue.addBulk([job]);
 }
 
+const hunyuanQueue = new Queue('hunyuanvideo', {
+  connection: redisClient,
+  defaultJobOptions: {
+    attempts: 1,
+    backoff: {
+      type: 'exponential',
+      delay: 1000,
+    },
+  },
+});
+
+async function runHunyuan(frames: string[], options) {
+  // console.log(`frames: ${JSON.stringify(frames)}`)
+  const job = {
+    name: 'message',
+    data: {
+      prompt: frames.join(),
+      frame_count: options.frame_count, // should be a multiple of the context window of 16
+      frame_rate: options.frame_rate,
+      seed: options.seed,
+      steps: options.steps,
+      width: options.width,
+      height: options.height,
+    },
+  };
+
+  console.log(`running: ${JSON.stringify(job)}`);
+  return hunyuanQueue.addBulk([job]);
+}
+
 // listen for job events and results. Note that it's best to deal with results directly in the worker
 const queueEvents = new QueueEvents('video');
 queueEvents.on('completed', async (data) => {
-  const job = await Job.fromId(videoQueue, data.jobId);
+  const job = await Job.fromId(hunyuanQueue, data.jobId);
   console.log(
     `\n${new Date().toISOString()}: Job finished: ${JSON.stringify(job?.returnvalue)} for job ${JSON.stringify(job)}`
   );
@@ -53,14 +92,77 @@ queueEvents.on('progress', (data) => {
   }
 });
 queueEvents.on('failed', async (data) => {
-  const job = await Job.fromId(videoQueue, data.jobId);
+  const job = await Job.fromId(hunyuanQueue, data.jobId);
   console.log(`\n${new Date().toISOString()}: Job failed:   ${job.failedReason} for job ${JSON.stringify(job)}`);
 });
 
-if (process.argv.length === 2) {
-  console.error('Expected at least one argument!');
-  process.exit(1);
+program.name('prompt').description('CLI to queue runpod jobs');
+
+function myParseInt(value) {
+  // parseInt takes a string and a radix
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new InvalidArgumentError('Not a number.');
+  }
+  return parsedValue;
 }
 
-const prompt = process.argv.slice(2).join(' ');
-await runVideo(prompt.split(','));
+program
+  .command('hunyuan')
+  .description('queue a runpod job')
+  .argument('<string>', 'prompt for hunyuan')
+  .option('-w, --width <number>', 'width', myParseInt, 1024)
+  .option('-h, --height <number>', 'height', myParseInt, 768)
+  .option(
+    '-c, --frame_count <number>',
+    'number of frames to compute, must be a multiple of four (after subtracting one',
+    myParseInt,
+    609
+  )
+  .option('-f, --frame_rate <number>', 'frame rate for video', myParseInt, 16)
+  .option('-s, --seed <number>', 'seed', myParseInt, 6)
+  .option(
+    '-t, --steps <number>',
+    'number of steps to generate a frame: more is slower but higher quality',
+    myParseInt,
+    30
+  )
+  .action((str, options) => {
+    runHunyuan(str.split(), options);
+  });
+
+program
+  .command('animatediff')
+  .description('queue a runpod job')
+  .argument('<string>', 'prompt for animatediff')
+  .option(
+    '-p, --pre_text <string>',
+    'Text that is prepended at the beginning of each prompt in the schedule, allowing for a consistent base across all scheduled prompts',
+    'highly detailed, 4k, masterpiece'
+  )
+  .option(
+    '-a, --app_text <string>',
+    'Text that is appended at the end of each prompt in the schedule, enabling a uniform conclusion to each prompt or adding consistent elements across prompts',
+    '(Masterpiece, best quality:1.2)  walking towards camera, full body closeup shot'
+  )
+  .option('-w, --width <number>', 'width', myParseInt, 1024)
+  .option('-h, --height <number>', 'height', myParseInt, 768)
+  .option(
+    '-c, --frame_count <number>',
+    'number of frames to compute, must be a multiple of four (after subtracting one',
+    myParseInt,
+    609
+  )
+  .option('-f, --frame_rate <number>', 'frame rate for video', myParseInt, 16)
+  .option('-s, --seed <number>', 'seed', myParseInt, 6)
+  .option(
+    '-t, --steps <number>',
+    'number of steps to generate a frame: more is slower but higher quality',
+    myParseInt,
+    30
+  )
+  .action((str, options) => {
+    runVideo(str.split(), options);
+  });
+
+program.parse();
