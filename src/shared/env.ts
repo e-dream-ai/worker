d42e04de8158fcb68d7c8e58173341732d97b29e
vuledev <77647824+mystixxx@users.noreply.github.com>
2025-09-18 20:10:13 +0200

    Update environment configuration.

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { bool, cleanEnv, port, str } from 'envalid';
 
 export const env = cleanEnv(process.env, {
-  NODE_ENV: str({ choices: ['development', 'test', 'production', 'stage'] }),
+  NODE_ENV: str({ choices: ['development', 'test', 'production', 'stage'], default: 'development' }),
   PORT: port({ default: 3000 }),
   DEBUG: bool({ default: false }),
 
@@ -14,12 +14,12 @@ export const env = cleanEnv(process.env, {
   REDIS_PORT: port({ default: 6379 }),
   REDIS_PASSWORD: str({ default: '' }),
 
-  RUNPOD_API_KEY: str(),
-  RUNPOD_HUNYUAN_ENDPOINT_ID: str(),
-  RUNPOD_ANIMATEDIFF_ENDPOINT_ID: str(),
-  RUNPOD_DEFORUM_ENDPOINT_ID: str(),
+  RUNPOD_API_KEY: str({ default: '' }),
+  RUNPOD_HUNYUAN_ENDPOINT_ID: str({ default: '' }),
+  RUNPOD_ANIMATEDIFF_ENDPOINT_ID: str({ default: '' }),
+  RUNPOD_DEFORUM_ENDPOINT_ID: str({ default: '' }),
 
-  ADMIN_PASS: str(),
+  ADMIN_PASS: str({ default: '' }),
 });
 
 export default env;
